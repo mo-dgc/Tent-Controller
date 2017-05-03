414266ff1f4facc8444136d8dd57f32cc2e695c7
Mo <grower.mo@gmail.com>
2017-05-03 20:15:35 +0000

    Simple fixes

--- www/index.php
+++ www/index.php
@@ -22,8 +22,8 @@
 
       <form class="form-signin">
         <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputUser" class="sr-only">Username</label>
+        <input type="string" id="inputUser" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox">
