bdacc92373081a4cc6a7907a50b54bf512b4be7c
Mo <grower.mo@gmail.com>
2017-05-23 10:55:18 -0500

    layout testing

--- www/dashboard/index.php
+++ www/dashboard/index.php
@@ -1,9 +1,14 @@
+<?php
+// $stream_url = "http://gtmcs.local:8765/picture/1/current/";
+// $stream_url = "/static/img/tomatoes.jpg";
+$stream_url = "http://pi0.local:8080/";
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
@@ -13,72 +18,42 @@
 
     <!-- Bootstrap core CSS -->
     <link href="/static/css/bootstrap.min.css" rel="stylesheet">
-    <link href="dashboard.css" rel="stylesheet">
+<link href="/static/css/font-awesome.min.css" rel="stylesheet">
+
   </head>
 <body>
-
-<div class="container">
-  <div class="row">
-    <div class="col-md-4 col-sm-5">
-      <div class="tabs-left">
-        <ul class="nav nav-tabs">
-          <li class="active"><a href="#a" data-toggle="tab"><span class="glyphicon glyphicon-heart"></span></a></li>
-          <li><a href="#b" data-toggle="tab"><span class="glyphicon glyphicon-star"></span></a></li>
-          <li><a href="#c" data-toggle="tab"><span class="glyphicon glyphicon-headphones"></span></a></li>
-          <li><a href="#d" data-toggle="tab"><span class="glyphicon glyphicon-time"></span></a></li>
-          <li><a href="#e" data-toggle="tab"><span class="glyphicon glyphicon-calendar"></span></a></li>
-          <li><a href="#f" data-toggle="tab"><span class="glyphicon glyphicon-cog"></span></a></li>
-        </ul>
-        <div class="tab-content">
-          <div class="tab-pane active" id="a">
-            <h3>Who do you Love?</h3>
-            <ul class="list-group pull-left">
-              <li class="list-group-item">
-                <h4>Jen &nbsp; &nbsp;<span class="badge pull-right">100%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Dezi &nbsp; &nbsp;<span class="badge pull-right">100%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Eli &nbsp; &nbsp;<span class="badge pull-right">100%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Mojo &nbsp; &nbsp;<span class="badge pull-right">83%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Myself &nbsp; &nbsp;<span class="badge pull-right">100%</span></h4>
-              </li>
-            </ul>
-          </div>
-          <div class="tab-pane" id="b">
-            <h3>What's your Favorite?</h3>
-            <ul class="list-group pull-left">
-              <li class="list-group-item">
-                <h4>Crystals &nbsp; &nbsp;<span class="badge pull-right">100%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Healing &nbsp; &nbsp;<span class="badge pull-right">90%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Exploring &nbsp; &nbsp;<span class="badge pull-right">78%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>QiGong &nbsp; &nbsp;<span class="badge pull-right">83%</span></h4>
-              </li>
-              <li class="list-group-item">
-                <h4>Myself &nbsp; &nbsp;<span class="badge pull-right">100%</span>
-                </h4></li>
-            </ul>
-          </div>
-          <div class="tab-pane" id="c">CCCCThirdamuno, ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae.</div>
-          <div class="tab-pane" id="d">DDDDDSecondo sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. Aliquam in felis sit amet augue.</div>
-          <div class="tab-pane" id="e">EEEEEThirdamuno, ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae.</div>
-          <div class="tab-pane" id="f">FFFFFFThirdamuno, ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. Quisque mauris augue, molestie tincidunt condimentum vitae.</div>
-        </div><!-- /tab-content -->
-      </div><!-- /tabbable -->
-    </div><!-- /col -->
-  </div><!-- /row -->
-</div><!-- /container -->
+<div class="containeri-fluid">
+<ul class="nav nav-pills nav-stacked col-sm-2 no-gutters">
+  <li class="active"><a href="#tab_a" data-toggle="pill"><span class="fa-stack fa-lg pull-left"><i class="fa fa-dashboard fa-stack-1x ">Pill A</a></li>
+  <li><a href="#tab_b" data-toggle="pill">Pill B</a></li>
+  <li><a href="#tab_c" data-toggle="pill">Pill C</a></li>
+  <li><a href="#tab_d" data-toggle="pill">Pill D</a></li>
+</ul>
+<div class="tab-content col-sm-10">
+        <div class="tab-pane active" id="tab_a">
+<div class="row">
+             <h4>Pane A</h4>
+<p><img width="600px" class="img-responsive img-rounded" src="<?php echo $stream_url; ?>" alt="Latest picture" /></p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+                ac turpis egestas.</p>
+        </div>
+</div>
+        <div class="tab-pane" id="tab_b">
+             <h4>Pane B</h4>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+                ac turpis egestas.</p>
+        </div>
+        <div class="tab-pane" id="tab_c">
+             <h4>Pane C</h4>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+                ac turpis egestas.</p>
+        </div>
+        <div class="tab-pane" id="tab_d">
+             <h4>Pane D</h4>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+                ac turpis egestas.</p>
+        </div>
+</div><!-- tab content -->
 
   <!-- Bootstrap core JavaScript
     ================================================== -->
@@ -86,6 +61,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery</script>
     <script src="/static/js/bootstrap.min.js"></script>
-    <script src="dashboard.js"></script>
 </body>
 </html>
