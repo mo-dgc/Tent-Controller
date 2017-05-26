a8304151d5bc7f953e76450fd560e4759530aaae
Mo <grower.mo@gmail.com>
2017-05-26 18:36:29 -0500

    Adding Ubuntu install

---
+++ installer/install.os.ubuntu.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+. $(dirname $(readlink -f $0))/funcs.sh
+
+# These are set per OS
+BINROOT="/usr/local/bin/"
+WEBROOT="/var/www/gtmcs/"
+APPUSER="root"
+
+
+apt_update_packages() {
+	msg "Calling apt-get update"
+	apt-get update
+}
+
+apt_upgrade_packages() {
+	msg "Calling apt-get upgrade"
+	apt-get -y upgrade
+}
+
+install_system_software() {
+	msg "Installing Avahi"
+	apt-get -y install avahi-daemon
+
+	msg "Installing Sqlite3"
+	apt-get -y install sqlite3
+
+	msg "Installing Python"
+	apt-get -y install python-dev python-pip python-setuptools
+	apt-get -y install python3 python3-dev python3-pip python3-setuptools python3-bcrypt python3-sqlalchemy python3-passlib
+
+	msg "Installing PHP7"
+	apt-get -y install php7.0 php7.0-curl php7.0-gd php7.0-fpm php7.0-cli php7.0-opcache php7.0-mbstring php7.0-xml php7.0-zip php7.0-sqlite3
+
+	msg "Installing Nginx"
+	apt-get -y install nginx
+
+	# Supervisor needed?
+	#msg "Installing Supervisor"
+	#apt-get -y install supervisor
+}
+
+install_components() {
+	msg "Installing components"
+	rsync --chown="$APPUSER":"$APPUSER" bin/* "$BINROOT"
+
+	mkdir "$WEBROOT"
+	cp -R www/* "$WEBROOT"
+
+	msg "Fixing permissions"
+	chown -R "$APPUSER":"$APPUSER" "$WEBROOT"
+}
+
+apt_upgrade_packages
+apt_update_preferences
+install_system_software
+
